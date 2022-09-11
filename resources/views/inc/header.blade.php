6e35c7e3c9a13285d845a528bada4c913e085554
Danylo <danilo.savchenko96@gmail.com>
2022-09-11 13:16:58 +0300

    auth

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -6,5 +6,8 @@
             <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('home') }}">Головна</a>
             <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('contact') }}">Контакти</a>
             <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('about') }}">Про нас</a>
+            <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('login') }}">Вхід</a>
+            <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('register') }}">Реєстрація</a>
+            <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('dashboard') }}">Особистий кабінет</a>
         </nav>
-    </div>
+</div>
