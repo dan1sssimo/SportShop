1d9e0a331aba265493559d166b0ec7571fec1df1
MelnykPavlo <pmelnik842@gmail.com>
2022-09-12 20:31:08 +0300

    dropdow

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,125 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!doctype html>
+<html lang="en">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<head>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+    <meta charset="utf-8"/>
+    <title>Register | SportShop </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description"/>
+    <meta content="Themesdesign" name="author"/>
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}">
+    <!-- Bootstrap Css -->
+    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet"
+          type="text/css"/>
+    <!-- Icons Css -->
+    <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css"/>
+    <!-- App Css-->
+    <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css"/>
 
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
+</head>
 
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+<body class="auth-body-bg">
+<div class="bg-overlay"></div>
+<div class="wrapper-page">
+    <div class="container-fluid p-0">
+        <div class="card">
+            <div class="card-body">
+
+                <div class="text-center mt-4">
+                    <div class="mb-3">
+                        <a href="/" class="auth-logo">
+                            <img src="{{asset('logo/logo.png')}}" width="75px">
+                        </a>
+                    </div>
+                </div>
+
+                <h4 class="text-muted text-center font-size-18"><b>Register</b></h4>
+
+                <div class="p-3">
+                    <!-- Session Status -->
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                    <!-- Validation Errors -->
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    <form class="form-horizontal mt-3" method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" type="text" required="" placeholder="Name" id="name"
+                                       name="name" >
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" type="text" required="" placeholder="Username" id="username"
+                                       name="username">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" type="email" required="" placeholder="Email" id="email"
+                                       name="email">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" type="password" required="" placeholder="Password"
+                                       id="password" name="password">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" type="password" required=""
+                                       placeholder="Password Confirmation" id="password_confirmation"
+                                       name="password_confirmation">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <div class="custom-control custom-checkbox"></div>
+                            </div>
+                        </div>
+
+                        <div class="form-group text-center row mt-3 pt-1">
+                            <div class="col-12">
+                                <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Register
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="form-group mt-2 mb-0 row">
+                            <div class="col-12 mt-3 text-center">
+                                <a href="{{route('login')}}" class="text-muted">Already have account?</a>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- end form -->
+                </div>
             </div>
+            <!-- end cardbody -->
+        </div>
+        <!-- end card -->
+    </div>
+    <!-- end container -->
+</div>
+<!-- end -->
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+<!-- JAVASCRIPT -->
+<script src="{{asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+<script src="{{asset('backend/assets/js/app.js')}}"></script>
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+//Admin All Route
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+});
+
+
+require __DIR__ . '/auth.php';
