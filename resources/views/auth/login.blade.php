35e68c92fed36a4bef09414e0024af861cbaffc6
Danylo <danilo.savchenko96@gmail.com>
2022-09-12 16:38:21 +0300

    front/back-end upDates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,104 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!doctype html>
+<html lang="en">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <meta charset="utf-8"/>
+    <title>Login | SportShop - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description"/>
+    <meta content="Themesdesign" name="author"/>
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}">
+    <!-- Bootstrap Css -->
+    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet"
+          type="text/css"/>
+    <!-- Icons Css -->
+    <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css"/>
+    <!-- App Css-->
+    <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css"/>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+</head>
 
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="username" :value="__('Username')" />
+<body class="auth-body-bg">
+<div class="bg-overlay"></div>
+<div class="wrapper-page">
+    <div class="container-fluid p-0">
+        <div class="card">
+            <div class="card-body">
 
-                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
+                <div class="text-center mt-4">
+                    <div class="mb-3">
+                        <a href="#" class="auth-logo">
+                            <img src="{{asset('logo/logo.png')}}" width="75px">
+                        </a>
+                    </div>
+                </div>
+
+                <h4 class="text-muted text-center font-size-18"><b>Sign In</b></h4>
+
+                <div class="p-3">
+                    <form class="form-horizontal mt-3" method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" type="text" required="" placeholder="UserName" id="username"
+                                       name="username">
+                            </div>
+                        </div>
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" id="password" name="password" autocomplete="current-password" type="password" required="" placeholder="Password">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="remember_me" name="remember">
+                                    <label class="form-label ms-1" for="customCheck1">Remember me</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 text-center row mt-3 pt-1">
+                            <div class="col-12">
+                                <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Log In
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-0 row mt-2">
+                            <div class="col-sm-7 mt-3">
+                                <a href="{{ route('password.request') }}" class="text-muted"><i class="mdi mdi-lock"></i> Forgot
+                                    your password?</a>
+                            </div>
+                            <div class="col-sm-5 mt-3">
+                                <a href="{{route('register')}}" class="text-muted"><i class="mdi mdi-account-circle"></i>
+                                    Create an account</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- end -->
             </div>
+            <!-- end cardbody -->
+        </div>
+        <!-- end card -->
+    </div>
+    <!-- end container -->
+</div>
+<!-- end -->
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+<!-- JAVASCRIPT -->
+<script src="{{asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+<script src="{{asset('backend/assets/js/app.js')}}"></script>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</body>
+</html>
