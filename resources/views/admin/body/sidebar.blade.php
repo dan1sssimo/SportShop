e069c4e25bfae73cc3feb9dea45435a2bef38097
Danylo <danilo.savchenko96@gmail.com>
2022-09-14 12:13:07 +0300

    upDated forms

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -9,7 +9,7 @@
                 <li class="menu-title">Menu</li>
 
                 <li>
-                    <a href="index.html" class="waves-effect">
+                    <a href="#" class="waves-effect">
                         <i class="ri-dashboard-line"></i><span
                             class="badge rounded-pill bg-success float-end">3</span>
                         <span>Dashboard</span>
@@ -17,7 +17,7 @@
                 </li>
 
                 <li>
-                    <a href="calendar.html" class=" waves-effect">
+                    <a href="#" class=" waves-effect">
                         <i class="ri-calendar-2-line"></i>
                         <span>Calendar</span>
                     </a>
@@ -29,39 +29,8 @@
                         <span>Email</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="email-inbox.html">Inbox</a></li>
-                        <li><a href="email-read.html">Read Email</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-layout-3-line"></i>
-                        <span>Layouts</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true">
-                        <li>
-                            <a href="javascript: void(0);" class="has-arrow">Vertical</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-dark-sidebar.html">Dark Sidebar</a></li>
-                                <li><a href="layouts-compact-sidebar.html">Compact Sidebar</a></li>
-                                <li><a href="layouts-icon-sidebar.html">Icon Sidebar</a></li>
-                                <li><a href="layouts-boxed.html">Boxed Layout</a></li>
-                                <li><a href="layouts-preloader.html">Preloader</a></li>
-                                <li><a href="layouts-colored-sidebar.html">Colored Sidebar</a></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a href="javascript: void(0);" class="has-arrow">Horizontal</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="layouts-horizontal.html">Horizontal</a></li>
-                                <li><a href="layouts-hori-topbar-light.html">Topbar light</a></li>
-                                <li><a href="layouts-hori-boxed-width.html">Boxed width</a></li>
-                                <li><a href="layouts-hori-preloader.html">Preloader</a></li>
-                                <li><a href="layouts-hori-colored-header.html">Colored Header</a></li>
-                            </ul>
-                        </li>
+                        <li><a href="#">Inbox</a></li>
+                        <li><a href="#">Read Email</a></li>
                     </ul>
                 </li>
 
@@ -73,10 +42,9 @@
                         <span>Authentication</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="auth-login.html">Login</a></li>
-                        <li><a href="auth-register.html">Register</a></li>
-                        <li><a href="auth-recoverpw.html">Recover Password</a></li>
-                        <li><a href="auth-lock-screen.html">Lock Screen</a></li>
+                        <li><a href="{{route('login')}}">Login</a></li>
+                        <li><a href="{{route('register')}}">Register</a></li>
+                        <li><a href="#">Recover Password</a></li>
                     </ul>
                 </li>
 
@@ -86,12 +54,9 @@
                         <span>Utility</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="pages-starter.html">Starter Page</a></li>
-                        <li><a href="pages-timeline.html">Timeline</a></li>
-                        <li><a href="pages-directory.html">Directory</a></li>
-                        <li><a href="pages-invoice.html">Invoice</a></li>
-                        <li><a href="pages-404.html">Error 404</a></li>
-                        <li><a href="pages-500.html">Error 500</a></li>
+                        <li><a href="{{route('home')}}">Starter Page</a></li>
+                        <li><a href="#">Error 404</a></li>
+                        <li><a href="#">Error 500</a></li>
                     </ul>
                 </li>
             </ul>
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,39 +1,93 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!doctype html>
+<html lang="en">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
+<head>
 
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-            </div>
-        @endif
+    <meta charset="utf-8"/>
+    <title>Verify Email | SportShop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description"/>
+    <meta content="Themesdesign" name="author"/>
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}">
+    <!-- Bootstrap Css -->
+    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet"
+          type="text/css"/>
+    <!-- Icons Css -->
+    <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css"/>
+    <!-- App Css-->
+    <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css"/>
 
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
+</head>
 
-                <div>
-                    <x-primary-button>
-                        {{ __('Resend Verification Email') }}
-                    </x-primary-button>
+<body class="auth-body-bg">
+<div class="bg-overlay"></div>
+<div class="wrapper-page">
+    <div class="container-fluid p-0">
+        <div class="card">
+            <div class="card-body">
+
+                <div class="text-center mt-4">
+                    <div class="mb-3">
+                        <a href="index.html" class="auth-logo">
+                            <img src="{{asset('logo/logo.png')}}" width="75px">
+                        </a>
+                    </div>
                 </div>
-            </form>
 
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
+                <h4 class="text-muted text-center font-size-18"><b>Verify Email</b></h4>
 
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
-                </button>
-            </form>
+                <div class="p-3">
+                    <div class="alert alert-info alert-dismissible fade show" role="alert">
+                        Thanks for signing up! Before getting started, could you verify your email address by clicking
+                        on
+                        the link we just emailed to you? If you didn't receive the email, we will gladly send you
+                        another.
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @if (session('status') == 'verification-link-sent')
+                        <div class="alert alert-info alert-dismissible fade show" role="alert">
+                            A new verification link has been sent to the email address you provided during registration.
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+                    <form class="form-horizontal mt-3" method="POST" action="{{ route('verification.send') }}">
+                        @csrf
+                        <div class="form-group pb-2 text-center row mt-3">
+                            <div class="col-12">
+                                <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Resend Verification Email
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <form class="form-horizontal mt-3" method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <div class="form-group pb-2 text-center row mt-3">
+                            <div class="col-12">
+                                <button class="btn btn-muted w-100 waves-effect waves-white" type="submit">Log Out
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- end cardbody -->
         </div>
-    </x-auth-card>
-</x-guest-layout>
+        <!-- end card -->
+    </div>
+    <!-- end container -->
+</div>
+<!-- end -->
+
+
+<!-- JAVASCRIPT -->
+<script src="{{asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
+
+<script src="{{asset('backend/assets/js/app.js')}}"></script>
+
+</body>
+</html>
